d6be7d39dd1cfa2dcf9d0d89a9b175c479362b32
Saurabh Pradhan(on Thinkstation) <pradhanphy@gmail.com>
2023-10-15 19:48:54 +0200

    Update config for indent-blankline

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -94,6 +94,15 @@ return require("packer").startup(function(use)
 			-- 	require("config.treesitter")
 			-- end,
 	})
+	use {
+			"danymat/neogen",
+			config = function()
+				require('neogen').setup {}
+			end,
+			requires = "nvim-treesitter/nvim-treesitter",
+			-- Uncomment next line if you want to follow only stable versions
+			-- tag = "*"
+	}
 	-- " TSInstallSync cpp bash cmake latex regex  vim lua python bibtex
 	-- Telescope
 	use({
