e4ba911aa54a5b200ba7d8a8a73359c49c807716
Saurabh Pradhan <saurabh@macair>
2025-04-04 10:23:39 +0200

    update github CI to update the plugins

--- install.sh
+++ install.sh
@@ -6,12 +6,13 @@ cd ~/.config/ || exit
 git clone https://github.com/srbhp/nvim.git
 sudo apt install clangd vim git cmake python3-venv luarocks tree-sitter-dev fd-find ripgrep gdb ninja-build
 # install neovim
-mkdir ~/.local/bin/
+mkdir -p ~/.local/bin/
 cd ~/.local/bin || exit
-wget https://github.com/neovim/neovim/releases/download/v0.11.0/nvim.appimage
-mv nvim.appimage nvim
+wget https://github.com/neovim/neovim/releases/download/v0.11.0/nvim-linux-x86_64.appimage
+mv nvim-linux-x86_64.appimage nvim
 chmod +x nvim
 export PATH=$PATH:~/.local/bin #  add ~/.local/bin to your PATH . Add this line to your ~/.bashrc
+cd ~/ || exit
 # run neovim to install plugins
 nvim +'lazy update' +q
 # tar all plugins
